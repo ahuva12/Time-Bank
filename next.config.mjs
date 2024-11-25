fc89105733f58ff551d215081a05776cde21f2fb
ahuva12 <ahuvaeshit@gmail.com>
2024-11-25 08:50:57 +0200

    initial

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    env: {
+        PUBLIC_DB_CONNECTION: process.env.MONGODB_URI,
+    },
+};
+
+export default nextConfig;
+
+
+  
