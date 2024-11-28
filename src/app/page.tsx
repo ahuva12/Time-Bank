d760ef6997f875600314a0e6a3690d4a38d57eea
ahuva12 <ahuvaeshit@gmail.com>
2024-11-28 15:43:02 +0200

    fwaz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Register from "../components/Login/Login";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/validations/validationsServer/activity.ts
+++ src/validations/validationsServer/activity.ts
@@ -17,13 +17,3 @@ export const updatingStatusSchema = z.object({
     status: z.enum(["proposed", "caught", "accepted", "cancelled"]),
 });
 
-export const addActivityForm = z.object({
-    nameActivity: z.string().min(1, "Activity name is required"),
-    durationHours: z.number().min(0, "Duration hours must be 0 or greater"),
-    tags: z.array(z.string().min(3, "Each tag must be at least 3 characters"))
-    .max(5, "You can only add up to 5 tags")
-    .optional()
-    .default([]),
-    description: z.string().min(1, "Description is required"),
-});
-
