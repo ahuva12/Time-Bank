fc58a8e0f4894c048544e58aa1cdc9e19a2f5fdf
ahuva12 <116567387+ahuva12@users.noreply.github.com>
2024-11-28 13:21:01 +0200

    Update activity.ts

--- src/validations/activity.ts
+++ src/validations/activity.ts
@@ -16,3 +16,13 @@ export const updatingStatusSchema = z.object({
     }).optional(), 
     status: z.enum(["proposed", "caught", "accepted", "cancelled"]),
 });
+
+export const addActivityForm = z.object({
+    nameActivity: z.string().min(1, "Activity name is required"),
+    durationHours: z.number().min(0, "Duration hours must be 0 or greater"),
+    tags: z.array(z.string().min(3, "Each tag must be at least 3 characters"))
+    .max(5, "You can only add up to 5 tags")
+    .optional()
+    .default([]),
+    description: z.string().min(1, "Description is required"),
+});
