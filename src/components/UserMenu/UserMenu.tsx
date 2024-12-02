90bf787289967100cb3e0db0eb4d736631e421f9
Naama Iluz <naamarin@g.jct.ac.il>
2024-12-02 14:07:50 +0200

    Some styles updates

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -38,6 +38,7 @@ export default function UserMenu({ logout }: { logout: Function }) {
                         <li onClick={() => handleRedirect('profile')}>פרופיל</li>
                         <li onClick={() => handleRedirect('history')}>היסטוריה</li>
                         <li onClick={() => handleRedirect('saved')}>שמורים</li>
+                        <li onClick={() => handleRedirect('myDonation')}>התרומה שלי</li>
                         <li onClick={handleLogout}>התנתקות</li>
                     </ul>
                 </div>
