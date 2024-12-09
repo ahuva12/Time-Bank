23fe8e0d703a61e09e00870765258c68cff6f58e
ElishevaFisher <7f2003@gmail.com>
2024-12-09 09:34:01 +0200

    add success message

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,20 +1,22 @@
+
 'use client';
-import { useState, FormEvent  } from "react";
+import { useState } from "react";
 import useUserStore from "@/store/useUserStore";
 import { useAuthStore } from '@/store/authStore';
 import Styles from './Login.module.css'
 import { useRouter } from 'next/navigation';
-import { useForm, SubmitHandler} from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { loginSchema } from '@/validations/validationsClient/user';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginUser } from '@/services/users';
+import SuccessMessage from '../SuccessMessage/SuccessMessage'; // Import the SuccessMessage component
 
 interface LoginProps {
   closePopup: () => void;
   setIsRegisterOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-type LoginFormFileds = {
+type LoginFormFields = {
   email: string;
   password: string;
 }
@@ -22,6 +24,7 @@ type LoginFormFileds = {
 const Login: React.FC<LoginProps> = ({ closePopup, setIsRegisterOpen }) => {
   const { login } = useAuthStore();
   const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState(""); 
   const setUser = useUserStore((state) => state.setUser);
   const router = useRouter();
 
@@ -29,19 +32,18 @@ const Login: React.FC<LoginProps> = ({ closePopup, setIsRegisterOpen }) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginFormFileds>({
-      resolver: zodResolver(loginSchema),
+  } = useForm<LoginFormFields>({
+    resolver: zodResolver(loginSchema),
   });
 
-  const onSubmit: SubmitHandler<LoginFormFileds> = async (data:LoginFormFileds) => {
+  const onSubmit: SubmitHandler<LoginFormFields> = async (data: LoginFormFields) => {
     try {
       const user = await loginUser(data.email, data.password);
       console.log(user)
       setUser(user); 
       login();
-      closePopup();
-      router.push('home'); 
-    } catch (error:any) {
+      setSuccessMessage("התחברת בהצלחה!"); 
+    } catch (error: any) {
       console.log(error)
       setError(error.data?.message || "An error occurred");
     }
@@ -52,40 +54,53 @@ const Login: React.FC<LoginProps> = ({ closePopup, setIsRegisterOpen }) => {
     setIsRegisterOpen(true); 
   };
 
+  // Function to handle the OK button click (close the success message and navigate to home)
+  const handleOkClick = () => {
+    closePopup(); // Close the login popup
+    router.push('home'); // Redirect to home page
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={Styles.container}>
-        <h1 className={Styles.title}>התחברות</h1>
-      <input className={Styles.inputFields}
-        type="email"
-        placeholder="אמייל"
-        {...register("email")}
-      />
-      {errors.email && <p>{String(errors.email.message)}</p>}
-      <input className={Styles.inputFields}
-        type="password"
-        placeholder="סיסמא"
-        {...register("password")}
-      />
-      {errors.password && <p>{String(errors.password.message)}</p>}
-      <div className={Styles.innerDiv}>
-        <button className={Styles.button} type="submit">כניסה</button>
-        <div style={{marginLeft: '50px'}}>
-      <p className={Styles.registerText}>
-          אין לך חשבון?{' '}
-          <span
-            style={{ color: 'blue', cursor: 'pointer', textDecoration: 'underline' }}
-            onClick={goRegister}
-          >
-            הירשם
-          </span>{' '}
-          במקום.
-        </p>
+    <>
+      {successMessage && <SuccessMessage 
+        message_line1={successMessage} 
+        message_line2="" 
+        onOkClick={handleOkClick} 
+      />}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={Styles.container}>
+          <h1 className={Styles.title}>התחברות</h1>
+          <input className={Styles.inputFields}
+            type="email"
+            placeholder="אמייל"
+            {...register("email")}
+          />
+          {errors.email && <p>{String(errors.email.message)}</p>}
+          <input className={Styles.inputFields}
+            type="password"
+            placeholder="סיסמא"
+            {...register("password")}
+          />
+          {errors.password && <p>{String(errors.password.message)}</p>}
+          <div className={Styles.innerDiv}>
+            <button className={Styles.button} type="submit">כניסה</button>
+            <div style={{ marginLeft: '50px' }}>
+              <p className={Styles.registerText}>
+                אין לך חשבון?{' '}
+                <span
+                  style={{ color: 'blue', cursor: 'pointer', textDecoration: 'underline' }}
+                  onClick={goRegister}
+                >
+                  הירשם
+                </span>{' '}
+                במקום.
+              </p>
+            </div>
+          </div>
+          {error && <p>{error}</p>}
         </div>
-      </div>
-      {error && <p>{error}</p>}
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
 
