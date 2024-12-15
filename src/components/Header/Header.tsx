fc20444968dd2d6988e9949b3654aa032e165388
ElishevaFisher <7f2003@gmail.com>
2024-12-15 12:54:09 +0200

    change pop-up style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,94 +1,90 @@
-'use client';
-import { useAuthStore } from '@/store/authStore';
-import styles from './Header.module.css';
-import Image from 'next/image';
-import logo from '../../../public/images/logo.gif';
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { Login, Register, UserMenu} from '@/components'; 
+"use client";
+import { useAuthStore } from "@/store/authStore";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import logo from "../../../public/images/logo.gif";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Login, Register, UserMenu } from "@/components";
 
 export default function Header() {
   const { isLoggedIn, login, signup } = useAuthStore();
-  const [isClient, setIsClient] = useState(false); 
+  const [isClient, setIsClient] = useState(false);
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isRegisterOpen, setIsRegisterOpen] = useState(false);
-  
+
   useEffect(() => {
     setIsClient(true);
-  }, [])
+  }, []);
 
   const toggleLogin = () => setIsLoginOpen((prev) => !prev);
   const toggleRegister = () => setIsRegisterOpen((prev) => !prev);
 
   if (!isClient) {
-    return null; 
+    return null;
   }
 
   if (isLoggedIn) {
     return (
-        <header className={styles.header}>
-            <nav className={styles.navigation}>
-              <UserMenu/>
+      <header className={styles.header}>
+        <nav className={styles.navigation}>
+          <UserMenu />
 
-              <Link href="/home" className={styles.navItem}>
-              דף הבית
-              </Link>
-              <Link href="/all_activities" className={styles.navItem}>
-              פעילויות
-              </Link>
-              <Link href="/give" className={styles.navItem}>
-                לתת
-              </Link>
-          </nav>
-        <Image className={styles.logo} src={logo} alt="Logo" width={200} height={100} />
+          <Link href="/home" className={styles.navItem}>
+            דף הבית
+          </Link>
+          <Link href="/all_activities" className={styles.navItem}>
+            פעילויות
+          </Link>
+          <Link href="/give" className={styles.navItem}>
+            לתת
+          </Link>
+        </nav>
+        <Image
+          className={styles.logo}
+          src={logo}
+          alt="Logo"
+          width={200}
+          height={100}
+        />
       </header>
     );
   } else {
     return (
-        <header className={styles.header}>
-          <nav className={styles.navigation}>
-            <button
-              className={styles.btnLogin}
-              onClick={toggleLogin}
-            >
-              כניסה
-            </button>
-            <button
-              className={styles.btnSignup}
-              onClick={toggleRegister}
-            >
-              הרשמה
-            </button>
-          </nav>
-        <Image className={styles.logo} src={logo} alt="Logo" width={200} height={100} />
+      <header className={styles.header}>
+        <nav className={styles.navigation}>
+          <button className={styles.btnLogin} onClick={toggleLogin}>
+            כניסה
+          </button>
+          <button className={styles.btnSignup} onClick={toggleRegister}>
+            הרשמה
+          </button>
+        </nav>
+        <Image
+          className={styles.logo}
+          src={logo}
+          alt="Logo"
+          width={200}
+          height={100}
+        />
 
         {/* Pop-up for Login */}
         {isLoginOpen && (
           <div className={styles.popup}>
-            <div className={styles.popupContent}>
-              <button
-                className={styles.closeButton}
-                onClick={toggleLogin}
-              >
-                ×
-              </button>
-              <Login closePopup={toggleLogin} setIsRegisterOpen={setIsRegisterOpen}/>
-            </div>
+            <Login
+              closePopup={toggleLogin}
+              setIsRegisterOpen={setIsRegisterOpen}
+            />
           </div>
         )}
 
         {/* Pop-up for Signup */}
         {isRegisterOpen && (
           <div className={styles.popup}>
-            <div className={styles.popupContent}>
-              <button
-                className={styles.closeButton}
-                onClick={toggleRegister}
-              >
-                ×
-              </button>
-              <Register closePopup={toggleRegister} setIsLoginOpen={setIsLoginOpen}/>
-            </div>
+            <Register
+              closePopup={toggleRegister}
+              setIsLoginOpen={setIsLoginOpen}
+            />
           </div>
         )}
       </header>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,13 @@
-'use client';
-import { useState, FormEvent  } from "react";
+"use client";
+import { useState, FormEvent } from "react";
 import { useUserStore } from "@/store/useUserStore";
-import { useAuthStore } from '@/store/authStore';
-import Styles from './Login.module.css'
-import { useRouter } from 'next/navigation';
-import { useForm, SubmitHandler} from "react-hook-form";
-import { loginSchema } from '@/validations/validationsClient/user';
+import { useAuthStore } from "@/store/authStore";
+import Styles from "./Login.module.css";
+import { useRouter } from "next/navigation";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { loginSchema } from "@/validations/validationsClient/user";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { loginUser } from '@/services/users';
+import { loginUser } from "@/services/users";
 
 interface LoginProps {
   closePopup: () => void;
@@ -17,7 +17,7 @@ interface LoginProps {
 type LoginFormFileds = {
   email: string;
   password: string;
-}
+};
 
 const Login: React.FC<LoginProps> = ({ closePopup, setIsRegisterOpen }) => {
   const { login } = useAuthStore();
@@ -31,63 +31,87 @@ const Login: React.FC<LoginProps> = ({ closePopup, setIsRegisterOpen }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormFileds>({
-      resolver: zodResolver(loginSchema),
+    resolver: zodResolver(loginSchema),
   });
 
-  const onSubmit: SubmitHandler<LoginFormFileds> = async (data:LoginFormFileds) => {
+  const onSubmit: SubmitHandler<LoginFormFileds> = async (
+    data: LoginFormFileds
+  ) => {
     try {
       const user = await loginUser(data.email, data.password);
-      console.log(user)
-      setUser(user); 
+      console.log(user);
+      setUser(user);
       login();
       closePopup();
-      router.push('home'); 
-    } catch (error:any) {
-      console.log(error)
+      router.push("home");
+    } catch (error: any) {
+      console.log(error);
       setError(error.data?.message || "An error occurred");
     }
   };
 
   const goRegister = () => {
     closePopup();
-    setIsRegisterOpen(true); 
+    setIsRegisterOpen(true);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={Styles.container}>
-        <h1 className={Styles.title}>התחברות</h1>
-      <input className={Styles.inputFields}
-        type="email"
-        placeholder="אמייל"
-        {...register("email")}
-      />
-      {errors.email && <p>{String(errors.email.message)}</p>}
-      <input className={Styles.inputFields}
-        type="password"
-        placeholder="סיסמא"
-        {...register("password")}
-      />
-      {errors.password && <p>{String(errors.password.message)}</p>}
-      <div className={Styles.innerDiv}>
-        <button className={Styles.button} type="submit">כניסה</button>
-        <div style={{marginLeft: '50px'}}>
-      <p className={Styles.registerText}>
-          אין לך חשבון?{' '}
-          <span
-            style={{ color: 'blue', cursor: 'pointer', textDecoration: 'underline' }}
-            onClick={goRegister}
-          >
-            הירשם
-          </span>{' '}
-          במקום.
-        </p>
+    <div className={Styles.container}>
+      <div className={Styles.closeButton} onClick={closePopup}>
+        &times;
+      </div>
+      <div className={Styles.heading}>התחברות</div>
+      <form onSubmit={handleSubmit(onSubmit)} className={Styles.form}>
+        <div className={Styles.fieldContainer}>
+          <input
+            className={Styles.input}
+            id="email"
+            type="email"
+            placeholder="אמייל"
+            {...register("email")}
+          />
+          {errors.email && (
+            <p className={Styles.errorMessage}>
+              {String(errors.email.message)}
+            </p>
+          )}
+        </div>
+        <div className={Styles.fieldContainer}>
+          <input
+            className={Styles.input}
+            id="password"
+            type="password"
+            placeholder="סיסמא"
+            {...register("password")}
+          />
+          {errors.password && (
+            <p className={Styles.errorMessage}>
+              {String(errors.password.message)}
+            </p>
+          )}
+        </div>
+        <input className={Styles.loginButton} type="submit" value="כניסה" />
+      </form>
+      <div className={Styles.socialAccountContainer}>
+        <span className={Styles.title}>או התחבר עם</span>
+        <div className={Styles.socialAccounts}>
+          <button className={Styles.socialButton}>
+            <svg
+              viewBox="0 0 488 512"
+              height="1em"
+              xmlns="http://www.w3.org/2000/svg"
+              className={Styles.svg}
+            >
+              <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+            </svg>
+          </button>
         </div>
       </div>
-      {error && <p>{error}</p>}
-      </div>
-    </form>
+      <span className={Styles.agreement}>
+        <a onClick={goRegister}>אין לך חשבון? הירשם</a>
+      </span>
+    </div>
   );
-}
+};
 
 export default Login;
