2f07a11fc30eef7467b837867d61d46fd8ef3ad6
Naama Iluz <naamarin@g.jct.ac.il>
2024-12-15 09:42:47 +0200

    Remove unnecessary ActivityModatForDonation component

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,6 @@ export { default as ActivityModal } from './ActivityModal/ActivityModal';
 export { default as Loader } from './Loader/Loader';
 export { default as ErrorMessage } from './ErrorMessage/ErrorMessage';
 export { default as SuccessMessage } from './SuccessMessage/SuccessMessage';
-export { default as ActivityModalForDonation } from './ActivityModalForDonation/ActivityModalForDonation';
 export { default as Register } from './Register/Register';
 export { default as MyDonation} from './MyDonation/MyDonation';
 export { default as ActivityPopUp} from './ActivityPopUp/ActivityPopUp';
